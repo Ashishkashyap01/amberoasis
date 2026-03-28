41ea9994d7633729b29bf120b503957386da51f0
Ashish Kashyap <ashishkashyapdos@gmail.com>
2026-03-28 16:36:39 +0530

    Add files via upload

---
+++ include/head.php
@@ -0,0 +1,20 @@
+<?php
+$companyName = "PureDrop Water Trading";
+?>
+<title><?php echo $pageTitle . " | " . $companyName; ?></title>
+
+<link rel="icon" href="assets/img/logo.png">
+<link rel="stylesheet" href="assets/css/style.css">
+<title><?php echo $companyName; ?></title>
+
+<link rel="icon" type="image/png" href="assets/img/logo.png">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="description" content="PureDrop Water provides pure drinking water supply in Gurugram and nearby areas.">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
+<link rel="stylesheet" href="assets/css/style.css">
