4db730558b7cd07c4c785fd8736ae33f79475c91
Ashish Kashyap <ashishkashyapdos@gmail.com>
2026-03-28 16:36:19 +0530

    Add top bar and main header to header.php

--- include/header.php
+++ include/header.php
@@ -1,1 +1,39 @@
+<!-- TOP BAR -->
 
+<div class="top-bar">
+
+<div class="top-left">
+📍 Gali No. 5, Near Shri Ram Public School Sector-73, Noida U.P. 201301
+</div>
+
+<div class="top-right">
+✉ customercare@himtatva.in
+<span>f</span>
+<span>i</span>
+<span>p</span>
+</div>
+
+</div>
+
+<!-- MAIN HEADER -->
+
+<header class="main-header">
+
+<div class="logo-box">
+<img src="assets/img/logoNew.png">
+</div>
+
+<div class="header-middle">
+📞 +91 8368900174
+</div>
+
+<nav class="navbar">
+<a href="#">HOME</a>
+<a href="#">ABOUT US</a>
+<a href="#">SUSTAINABILITY</a>
+<a href="#">BULK SUPPLY</a>
+<a href="#">DISTRIBUTION</a>
+<a href="#">CONTACT</a>
+</nav>
+
+</header>
